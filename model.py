2606e74426f03c9926e1350c685119b9f3e7583c
JiauZhang <1743960454@qq.com>
2023-04-26 07:18:09 +0800

    update

---
+++ model.py
@@ -0,0 +1,22 @@
+from torch import nn
+
+class Encoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, input):
+        return input
+
+class StyleTrans(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, input):
+        return input
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, input):
+        return input
